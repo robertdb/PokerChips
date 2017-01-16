6138989a0ed507e72aa3cbbf66be0f3d787c1c61
Marcos <mpozzo94@gmail.com>
2017-01-16 03:09:49 -0300

    Rename de ExampleUnitTest a MesaTest

--- app/src/test/java/ram/fichaspokerapp/MesaTest.java
+++ app/src/test/java/ram/fichaspokerapp/MesaTest.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
-public class ExampleUnitTest {
+public class MesaTest {
 
     private Administrador unAdministrador = new Administrador();
     private Jugador unJugador = new Jugador();
