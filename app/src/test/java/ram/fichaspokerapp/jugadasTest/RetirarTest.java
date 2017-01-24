9f1f387f6b369676bb97d794a0f7063a8959ac5f
Robert <robertdibejar@gmail.com>
2017-01-23 22:41:56 -0300

    pasarConUnaSubidaDeAlgunoDeLaMesaDeJuegoEsUnError()

--- app/src/test/java/ram/fichaspokerapp/jugadasTest/RetirarTest.java
+++ app/src/test/java/ram/fichaspokerapp/jugadasTest/RetirarTest.java
@@ -28,4 +28,29 @@ public class RetirarTest {
 
     }
 
+    @Test(expected = NoSePuedePasarSiSeSubeError.class)
+    public void pasarConUnaSubidaDeAlgunoDeLaMesaDeJuegoEsUnError(){
+
+        // jugador 1 sube la apuesta.
+        Jugada subida = new Subir();
+
+        // jugador 2 se retira.
+        Jugada retirada1 = new Retirar(subida);
+
+        //jugador 3 se retira.
+        Jugada retirada2 = new Retirar(retirada1);
+
+        //jugador 4 se retira.
+        Jugada retirada3 = new Retirar(retirada2);
+
+        //jugador 5 se retira.
+        Jugada retirada4 = new Retirar(retirada3);
+
+        // jugador  intenta pasar y no puede ya que hubo una subida previa.
+        Jugada jugada = new Pasar(retirada4);
+
+    }
+
+
+
 }
