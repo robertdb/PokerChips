329ea63650b6e12629be55477702c71eeb25589b
Marcos <mpozzo94@gmail.com>
2017-01-29 04:32:42 -0300

    Jugando un poco con las vistas antes del Federer vs. Nadal.

--- app/src/main/java/ram/fichaspokerapp/controlador/MainActivity.java
+++ app/src/main/java/ram/fichaspokerapp/controlador/MainActivity.java
@@ -2,14 +2,28 @@ package ram.fichaspokerapp.controlador;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import ram.fichaspokerapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int cantidad = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void apostar(View vista) {
+        this.cantidad ++;
+        mostrar(cantidad);
+    }
+
+    private void mostrar(int numero) {
+        TextView quantityTextView = (TextView) findViewById(R.id.laCantidad);
+        quantityTextView.setText("" + numero);
     }
 }
