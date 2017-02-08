66cbd0543d6f95bbac0d482aa5f5744fb631875f
Robert <robertdibejar@gmail.com>
2017-02-08 01:43:19 -0300

    refactor de la clase abstracta Ronda, se le elimina el metodo comprobaraGanadore, la mano puede resolver este metodo sin necesida de la clase Ronda

--- app/src/main/java/ram/fichaspokerapp/modelo/Ronda.java
+++ app/src/main/java/ram/fichaspokerapp/modelo/Ronda.java
@@ -9,12 +9,6 @@ public abstract class Ronda {
 
     public abstract Ronda rondaTerminada();
 
-    public boolean comprobarGanador(IteradorListaCircular candidatosIter, Pozo pozo) {
-
-        return true;
-
-    }
-
     public boolean cambiarRonda(Agresor agresor, Jugador proximoJugador){
 
         return (!agresor.seguirRonda(proximoJugador));
